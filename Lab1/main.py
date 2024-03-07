7feb6d78a0fe0af13b5a0de95f041cd2ed9df1b9
BaciuMaria <100144074+BaciuMaria@users.noreply.github.com>
2024-03-07 16:25:48 +0200

    Add files via upload

---
+++ Lab1/main.py
@@ -0,0 +1,76 @@
+import math
+import random
+
+# Exercitiul 1
+# Cautam precizia masinii
+
+u = 10
+while  1 + float(u/10) != 1.0 :
+    u =float(u /10)
+
+print(f"Exercitiul 1 :",u)
+print(1 + 0.000000000000001) # u = 10^(-15)
+print(1 + 0.0000000000000001)
+
+# Exercitiul 2
+
+x= 1.0
+y= float(u/10)
+z= float(u/10)
+
+suma1= (x +y) + z
+suma2=x +(y+z)
+print("\nExercitiul 2\n")
+print(f"Suma 1 = ",suma1)
+print(f"Suma 2 = ",suma2)
+if (suma1 != suma2):
+    print("Operatia de adunare nu este asociativa.")
+else : print("Operatia de adunare este asociativa.")
+
+a = 0.2
+b = 0.4
+c = float(1/3)
+
+mult1= (a *b) * c
+mult2=a *(b*c)
+print(f"Produsul 1 = ",mult1)
+print(f"Produsul 2 = ",mult2)
+if (mult1 != mult2):
+    print("Operatia de inmultire nu este asociativa.")
+else : print("Operatia de inmultire este asociativa.")
+
+# Exercitiul 3
+print("\nExercitiul 3\n")
+a = random.uniform(-math.pi/2,math.pi/2)
+standard = float(math.tan(a))
+print(standard)
+# i = 4
+t4=float( (105 * a - 10 * (a**3)) / ( 105 - 45* (a**2) + a**4))
+print(t4)
+# i = 5
+t5=float( (945 * a - 105 * (a**3) + a**5) / ( 945 - 420* (a**2) + 15 *(a**4)))
+print(t5)
+# i = 6
+t6=float( (10395 * a - 1260 * (a**3) + 21*(a**5)) / ( 10395 - 4725* (a**2) + 210 *(a**4) - a**6))
+print(t6)
+# i = 7
+t7 = float((135135*a - 17325*(a**3) + 378*(a**5) - a**7)/(135135 - 62370*(a**2) + 3150*(a**4) - 28*(a**6)))
+print(t7)
+# i = 8
+t8 = float((2027025*a - 17325*(a**3) + 378*(a**5) - a**7)/ (2027025 - 945945*(a**2) + 51975*(a**4) - 630*(a**6) + a**8))
+print(t8)
+# i = 9
+t9 = float((34459425*a - 4729725*(a**3) + 135135*(a**5) - 990*(a**7) + a**9)/ (34459425 - 16216200*(a**2) + 945945*(a**4) - 13860*(a**6) + 45*(a**8)))
+print(t9)
+
+error4= standard - t4
+error5= standard - t5
+error6= standard - t6
+error7= standard - t7
+error8= standard - t8
+error9= standard - t9
+
+list_error = {'t4': error4 , 't5':error5 ,'t6':error6 ,'t7':error7 ,'t8':error8 , 't9': error9}
+print(f"Lista erorilor nesortata:",list_error)
+sorted_list_error = dict(sorted(list_error.items(),key= lambda x: abs(x[1])))
+print(f"Lista erorilor sortata:",sorted_list_error)
